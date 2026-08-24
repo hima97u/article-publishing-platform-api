aa6b242c3651e08b2d6827b9a684f8ba0e7125e6
hima97u <work.himan7u@gmail.com>
2026-08-25 00:01:18 +0530

    setup basic django project

---
+++ authors_api/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *  # noqa
+from .base import env
+
+ADMINS = [("Himanhsu", "work.himanshu62@gmail.com")] # this is for
+
+# TODO add domain names of the production server
+CSRF_TRUSTED_ORIGINS = [""]
