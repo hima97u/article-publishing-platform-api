2ae4217569ffde43d65d75433ba9b1b187c386f0
hima97u <work.himan7u@gmail.com>
2026-09-04 00:35:42 +0530

    Added TimeStamped and profile models, along with signals for user to create profile

--- core_apps/profiles/apps.py
+++ core_apps/profiles/apps.py
@@ -5,5 +5,8 @@ from django.utils.translation import gettext_lazy as _
 class ProfilesConfig(AppConfig):
     name = "core_apps.profiles"
     verbose_name = _("Profiles")
+    
+    def ready(self):
+        from core_apps.profiles import signals
 
     
