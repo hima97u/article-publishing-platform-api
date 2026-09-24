056dd5f6314964eb37f6ee7d4b5ac900fc64e3b6
hima97u <work.himan7u@gmail.com>
2026-09-25 00:38:16 +0530

    feat: implement article clap feature

--- core_apps/articles/models.py
+++ core_apps/articles/models.py
@@ -11,6 +11,18 @@ from .read_time_engine import ArticleReadTimeEngine
 User = get_user_model()
 
 
+class Clap(TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey("Article", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ["user", "article"]
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.user.first_name} clapped {self.article.title}"
+    
+
 class Article(TimeStampedModel):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
     title = models.CharField(verbose_name=_("Title"), max_length=255)
@@ -21,6 +33,9 @@ class Article(TimeStampedModel):
         verbose_name=_("banner image"), default="/profile_default.png"
     )
     tags = TaggableManager()
+    
+    # a user can clap multiple times for an article and also an article can have multiple claps from different users
+    claps = models.ManyToManyField(User, through=Clap, related_name="clapped_articles")
     
     def __str__(self):
         return f"{self.author.first_name}'s article"
--- core_apps/articles/urls.py
+++ core_apps/articles/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     ArticleListCreateView,
     ArticleRetrieveUpdateDestroyView,
+    ClapArticleView,
 )
 
 urlpatterns = [
@@ -11,5 +12,10 @@ urlpatterns = [
         "<uuid:id>/",
         ArticleRetrieveUpdateDestroyView.as_view(),
         name="article-retrieve-update-destroy",
+    ),
+    path(
+        "<uuid:article_id>/clap/",
+        ClapArticleView.as_view(),
+        name="clap-article",
     ),
 ]
